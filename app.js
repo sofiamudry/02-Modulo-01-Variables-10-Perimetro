c803fd1ab350bc3bfd843e36e8bad8edaa81be01
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 14:08:21 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Perimetro');
+
+alert('Hola, Bienvenide!');
+
+const altura = prompt('Ingrese la altura');
+const ancho = prompt('Ingrese el ancho');
+const perimetro = (altura*2+ancho*2);
+
+alert(`El perimetro del rectangulo es: ${perimetro}`);
